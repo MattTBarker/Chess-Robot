f2ccc0ee120c461dbfacda3f45aaa9fd1cad5a3c
MattTBarker <45687441+MattTBarker@users.noreply.github.com>
2019-02-01 17:48:15 +0000

    Update BoardRecog.py

--- BoardRecog.py
+++ BoardRecog.py
@@ -7,38 +7,155 @@ import os
 
 #Constants
 PATH='C:/Users/Matt/Desktop/Chessboards/'
-HARRISBLOCKSIZE=4
+HARRISBLOCKSIZE=3
 HARRISAPERTURE=3
-HARRISCORNERRESPONSE=0.1
-HARRISFILTER=0.1
+HARRISCORNERRESPONSE=0.02
+HARRISFILTER=0.08
+MINCORNERDISTANCE=0.05
+CLEANEDGEBLOCKSIZE=10
+CLEANEDGEBIAS=1
+
 
 def cycleImg(img):
     cv2.imshow('chessboard',img)
     if cv2.waitKey(0):
         cv2.destroyAllWindows()
 
+def getMergedCentroidClusters(centroids):
+    corners = list({(int(l[0]), int(l[1])) for l in centroids})
+
+    distance = np.ceil(MINCORNERDISTANCE*min(img.shape[:2]))
+    while True:
+        flag=True
+        
+        size=len(corners)
+        i=0
+        while i < size:
+            corner1 = corners[i]
+            for corner in corners:
+                if corner==corner1:
+                    continue
+                if abs(corner[0]-corner1[0])<distance and abs(corner[1]-corner1[1])<distance:
+                    corners.remove(corner)
+                    corner1=(corner[0]+corner1[0]//2, corner[1]+corner1[1]//2)
+                    size-=1
+                    flag=False
+            i+=1
+        if(flag):
+            break
+    return corners
+
+def getFilteredCentroids(img, centroids, samplingRate, threshold, imgTest):
+    cv2.dilate(img, np.ones((100, 100)), iterations=10)
+
+    height, width = img.shape[:2]
+    distance = 0.1*max(img.shape[:2])
+
+    centroids = [corner for corner in centroids if corner[0]!=0 and corner[0]!=width and corner[1]!=0 and corner[1]!=height]
+
+    height-=1
+    width-=1
+
+    lines=[]
+
+    for corner in centroids:     
+        for corner1 in centroids:     
+            if abs(corner[0]-corner1[0])<distance and abs(corner[1]-corner1[1])<distance:
+                continue
+
+            rho = None
+            edgeIntercept = None
+            edgeIntercept1 = None
+
+            if corner[0]-corner1[0]==0:
+                rho=0.5*np.pi
+                edgeIntercept = (corner[0], 0)
+                edgeIntercept1 = (corner[0], height)
+            else: 
+                m=(corner[1]-corner1[1])/(corner[0]-corner1[0])
+                c=corner[1]-m*corner[0]
+                rho=np.arctan2(corner[1]-corner1[1], corner[0]-corner1[0])        
+            
+                if c >= 0 and c <= height:  #intersection check for line X=0
+                    edgeIntercept = (0, c)
+                elif m*width + c >=0 and m*width + c <= height: #intersection check for line X=width
+                    edgeIntercept = (width, m*width + c)
+                elif -c/m >= 0 and -c/m <= width: #intersection check for line Y=0
+                    edgeIntercept = (-c/m, 0)
+
+                if c >= 0 and c <= height and edgeIntercept != (0, c):
+                    edgeIntercept1 = (0, c)
+                elif m*width + c >=0 and m*width + c <= height and edgeIntercept != (width, m*width + c):
+                    edgeIntercept1 = (width, c)
+                elif -c/m >= 0 and -c/m <= width and edgeIntercept != (-c/m, 0):
+                    edgeIntercept1 = (-c/m, 0)
+                else: #must intersect at Y=height
+                    edgeIntercept1 = ((height-c)/m, height)
+
+            xStep = (edgeIntercept1[0]-edgeIntercept[0])/samplingRate
+            yStep = (edgeIntercept1[1]-edgeIntercept[1])/samplingRate
+
+            edgeIntercept = tuple(map(int,edgeIntercept))
+            edgeIntercept1 = tuple(map(int,edgeIntercept1))
+
+            lineValue=0
+
+            for i in range(samplingRate):
+                x=int(edgeIntercept[0]+i*xStep)
+                y=int(edgeIntercept[1]+i*yStep)
+                lineValue += img[y, x]
+                lineValue -= CLEANEDGEBIAS*cv2.mean(img[max(0,y-CLEANEDGEBLOCKSIZE):min(height,y+CLEANEDGEBLOCKSIZE), max(0,x-CLEANEDGEBLOCKSIZE):min(width,x+CLEANEDGEBLOCKSIZE)])[0]
+
+            lines.append((edgeIntercept, edgeIntercept1, lineValue, rho))
+            
+
+##    angles = [[1, lines[0][3]]]
+##
+##    for line in lines:
+##        flag=False
+##        i=0
+##        while i<len(angles):
+##            if abs(line[3]-angles[i][1])%(2*np.pi)<0.05*np.pi:
+##                flag=True
+##                break
+##            i+=1
+##        if flag:
+##            angles[i][1]=angles[i][1]+line[3]
+##            angles[i][1]%=2*np.pi
+##            angles[i][1]/=2
+##            angles[i][0]+=1
+##        else:
+##            angles.append([1, line[3]])
+##
+##    angle=max(angles, key=lambda x: x[0])[1]
+##    angles = [x for x in angles if abs(angle-x[1])%(2*np.pi)>0.35*np.pi]
+##    angle1=max(angles, key=lambda x: x[0])[1]
+##            
+##    lines = [line for line in lines if abs(angle-line[3])%(2*np.pi)<0.15*np.pi or abs(angle1-line[3])%(2*np.pi)<0.15*np.pi]
+
+    maxLineValue=max(lines, key=lambda x: x[2])[2]*threshold
+    for line in lines:
+        if line[2] >= maxLineValue:
+            cv2.line(imgTest,line[0],line[1],(0,0,255),2)
+    cycleImg(imgTest)
+    cycleImg(img)
+        
+
 for filename in os.listdir(PATH):  
     img = cv2.imread(PATH + filename)
-    img = cv2.copyMakeBorder(img, 10, 10, 10, 10, cv2.BORDER_CONSTANT, value=[0,255,0])
     cycleImg(img)
+
     imgGrey = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-
     imgGrey = np.float32(imgGrey)
-    cornermap = cv2.cornerHarris(imgGrey,HARRISBLOCKSIZE,HARRISAPERTURE,HARRISCORNERRESPONSE)
 
-    img[cornermap>HARRISFILTER*cornermap.max()]=[0,0,255]
-    cycleImg(img)
+    cornerMap = cv2.cornerHarris(imgGrey,HARRISBLOCKSIZE,HARRISAPERTURE,HARRISCORNERRESPONSE)
+    cornerMap = cv2.threshold(cornerMap,0.04*cornerMap.max(),255,cv2.THRESH_BINARY)[1]
+    cornerMap = np.uint8(cornerMap)
 
-    img = cv2.Canny(img,50,150,apertureSize = 3)
-    cycleImg(img)
-    
-    height, width = img.shape
-    img=np.zeros((height,width,3), np.uint8)
-    img[cornermap>0.04*cornermap.max()]=[255,255,255]
-    cycleImg(img)
+    centroidArray = cv2.connectedComponentsWithStats(cornerMap)[3]
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
 
-    img = cv2.erode(img, None)
-    img = cv2.dilate(img, None)
+    imgCanny = cv2.Canny(img,50,150,apertureSize = 3)
 
-    cycleImg(img)
-
+    corners = getMergedCentroidClusters(centroidArray)
+    getFilteredCentroids(imgCanny, corners, 100, 0.4, img)
